fef7ab1a36c9967b46411b98d0decb13c1e43981
chandan123457 <chandankumarsang06@gmail.com>
2025-03-15 15:27:49 +0530

    file is added

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      backgroundColor: "#ffcccb",
+      color: "#333",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      <h1 style={{ fontSize: "3rem" }}>🎉 Happy Birthday, Harkirat! 🎂</h1>
+      <p style={{ fontSize: "1.5rem" }}>Wishing you a fantastic day ahead! 🎈🥳</p>
+    </div>
+  );
+}
